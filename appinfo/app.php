c00a2a02eee4ca5d569dafd36fe077b977b73ee9
Thomas Müller <thomas.mueller@tmit.eu>
2016-06-30 14:27:18 +0200

    Move to new backend registration

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,16 +1,8 @@
 <?php
 
-$l = \OC::$server->getL10N('files_external_ftp');
-
-OC_Mount_Config::registerBackend('\OCA\Files_External_FTP\FTP', [
-	'backend' => (string)$l->t('FTP (Fly)'),
-	'priority' => 100,
-	'configuration' => [
-		'host' => (string)$l->t('hostname'),
-		'username' => (string)$l->t('Username'),
-		'password' => (string)$l->t('Password'),
-		'root' => '&' . $l->t('Remote subfolder'),
-		'ssl' => '!' . $l->t('Secure ftps://'),
-		'port' => '&' . $l->t('Port'),
-	],
-]);
+\OC::$server->getEventDispatcher()->addListener(
+	'OCA\\Files_External::loadAdditionalBackends', function($event) {
+	$backendService = \OC::$server->getStoragesBackendService();
+	$backendService->registerBackendProvider(new \OCA\Files_External_FTP\BackendProvider());
+}
+);
