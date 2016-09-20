0876f53c64c76ab5640a9cb1d382aa216fcf5bb6
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-09-20 09:07:18 +0200

    [tx-robot] updated from transifex

---
+++ l10n/el.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "files_external_ftp",
+    {
+    "hostname" : "όνομα κεντρικού υπολογιστή",
+    "Username" : "Όνομα χρήστη",
+    "Password" : "Κωδικός πρόσβασης",
+    "Remote subfolder" : "Απομακρυσμένος υποφάκελος",
+    "Secure ftps://" : "Ασφαλής ftps://",
+    "Port" : "Θύρα"
+},
+"nplurals=2; plural=(n != 1);");
