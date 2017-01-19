fc4feac1c5a3f10dea18b7571d4bc0406b95db0c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-01-19 01:05:20 +0100

    [tx-robot] updated from transifex

---
+++ l10n/nn_NO.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "files_external_ftp",
+    {
+    "FTP (Fly)" : "FTP (Fly)",
+    "Username" : "Brukarnamn",
+    "Password" : "Passord",
+    "Host" : "Tenar",
+    "Root" : "Rot",
+    "Port" : "Port",
+    "Secure ftps://" : "Sikker ftp med ftps://"
+},
+"nplurals=2; plural=(n != 1);");
