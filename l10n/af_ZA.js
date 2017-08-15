ab18e8a6d5d8fed97b004fc0cb0cb28704eda918
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-15 02:12:20 +0000

    [tx-robot] updated from transifex

---
+++ l10n/af_ZA.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "files_external_ftp",
+    {
+    "FTP (Fly)" : "FTP (Fly)",
+    "Username" : "Gebruikersnaam",
+    "Password" : "Wagwoord",
+    "Host" : "Gasheer",
+    "Root" : "Wortel",
+    "Port" : "Poort",
+    "Secure ftps://" : "Beveiligde ftps://"
+},
+"nplurals=2; plural=(n != 1);");
