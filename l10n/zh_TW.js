8e83406bf98dd5895df8867abf3adaf933dede6f
Thomas Müller <thomas.mueller@tmit.eu>
2020-09-23 00:07:40 +0000

    [tx] updated from transifex

---
+++ l10n/zh_TW.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "files_external_ftp",
+    {
+    "Username" : "使用者名稱",
+    "Password" : "密碼",
+    "Host" : "主機",
+    "Root" : "根目錄",
+    "Port" : "連接埠",
+    "Secure ftps://" : "安全 ftps://"
+},
+"nplurals=1; plural=0;");
