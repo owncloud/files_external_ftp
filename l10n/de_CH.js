ad4ad6b23c3e378d3d5d0aef8d8278a3fbb0f914
Thomas Müller <thomas.mueller@tmit.eu>
2020-10-31 00:05:15 +0000

    [tx] updated from transifex

---
+++ l10n/de_CH.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "files_external_ftp",
+    {
+    "FTP (Fly)" : "FTP (Fly)",
+    "Username" : "Benutzername",
+    "Password" : "Passwort",
+    "Host" : "Host",
+    "Root" : "Root",
+    "Port" : "Port",
+    "Secure ftps://" : "Sicherer ftps://"
+},
+"nplurals=2; plural=(n != 1);");
