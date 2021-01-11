5ed7e6b0f00609db30aef9bd7194ce6ea98101d4
Jannik Stehle <jannik.stehle@gmail.com>
2021-01-11 15:27:05 +0100

    Require core test bootstrap.php file

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -3,6 +3,7 @@
 \define('PHPUNIT_RUN', 1);
 
 require_once __DIR__ . '/../../../../lib/base.php';
+require_once __DIR__ . '/../../../../tests/bootstrap.php';
 
 \OC::$composerAutoloader->addPsr4('Test\\', OC::$SERVERROOT . '/tests/lib/', true);
 \OC::$composerAutoloader->addPsr4('Tests\\', OC::$SERVERROOT . '/tests/', true);
