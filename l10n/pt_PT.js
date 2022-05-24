ab59aab650fcb5716ce5da8fa450298b85b2e8d7
ownClouders <devops@owncloud.com>
2022-05-24 00:36:18 +0000

    [tx] updated from transifex

--- l10n/pt_PT.js
+++ l10n/pt_PT.js
@@ -9,4 +9,4 @@ OC.L10N.register(
     "Port" : "Porta",
     "Secure ftps://" : "ftps:// Seguro"
 },
-"nplurals=2; plural=(n != 1);");
+"nplurals=3; plural=(n == 0 || n == 1) ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
