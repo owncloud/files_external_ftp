424c5f6a7028b70b176e733255f2285cf3c77e3e
ownClouders <devops@owncloud.com>
2024-10-14 01:39:04 +0000

    [tx] updated from transifex

---
+++ l10n/ug.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "files_external_ftp",
+    {
+    "FTP (Fly)" : "FTP (Fly)",
+    "Username" : "ئىشلەتكۈچى ئاتى",
+    "Password" : "ئىم",
+    "Host" : "باش ئاپپارات",
+    "Root" : "يىلتىز",
+    "Port" : "ئېغىز",
+    "Secure ftps://" : "بىخەتەر ftps: //"
+},
+"nplurals=2; plural=(n != 1);");
